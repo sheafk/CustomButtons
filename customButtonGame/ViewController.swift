ea03aff30053ddaf110ecc276153af246ac2ff2b
sheafk <sheafureyking@gmail.com>
2016-09-23 15:28:23 -0400

    Initial Commit

---
+++ customButtonGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  customButtonGame
+//
+//  Created by Flatiron School on 9/23/16.
+//  Copyright © 2016 Shea Furey-King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
