02a3e9d1af5bc6711cd771a1d3a7ed9be19adb5c
sheafk <sheafureyking@gmail.com>
2016-09-24 11:09:40 -0400

    first commit

---
+++ MysteryButton.swift
@@ -0,0 +1,21 @@
+//
+//  MysteryButton.swift
+//  customButtonGame
+//
+//  Created by Flatiron School on 9/23/16.
+//  Copyright © 2016 Shea Furey-King. All rights reserved.
+//
+
+import UIKit
+
+class MysteryButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
